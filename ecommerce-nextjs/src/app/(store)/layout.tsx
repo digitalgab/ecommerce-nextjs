0a6b007fd68ae729fef529f674a57f3afc6683d8
Digital GaB <digitallgab@gmail.com>
2025-02-08 21:04:36 -0300

    Adiciona api e rotas internas

--- ecommerce-nextjs/src/app/(store)/layout.tsx
+++ ecommerce-nextjs/src/app/(store)/layout.tsx
@@ -1,4 +1,4 @@
-import { Header } from "@/components/(header)";
+import { Header } from "@/app/components/(header)";
 
 export default function StoreLayout({ children }: { children: React.ReactNode }) {
     return (
