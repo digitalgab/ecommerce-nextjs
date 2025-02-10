93ea321cc42c1eea5ae6d932ad28ea7e440d4f51
Digital GaB <digitallgab@gmail.com>
2025-02-10 13:58:59 -0300

    Chamada a api para busca de produtos

---
+++ ecommerce-nextjs/src/app/api/products/search/route.ts
@@ -0,0 +1,14 @@
+import type { NextRequest } from 'next/server'
+import { z } from 'zod'
+import data from '../data.json'
+
+export async function GET(request: NextRequest) {
+    const { searchParams } = request.nextUrl
+    const query = z.string().parse(searchParams.get('q'))
+    const filteredProducts = data.products.filter(product => {
+        product.title.toLowerCase().includes(query.toLowerCase())
+    })
+
+    return Response.json(filteredProducts)
+}
+
--- ecommerce-nextjs/src/app/components/(header)/(components)/search-bar.tsx
+++ ecommerce-nextjs/src/app/components/(header)/(components)/search-bar.tsx
@@ -1,12 +1,33 @@
+'use client'
+
 import { Search } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation';
 
 export function SearchBar() {
+
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
+  const query = searchParams.get('q')
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const query = formData.get("q");
+  
+    router.push(`/products/search?q=${query}`);
+  }
+
   return (
-    <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
+    <form onSubmit={handleSubmit}
+    className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
       <Search className="h-5 w-5 text-zinc-500" />
 
       <input
-        placeholder="Search products..."
+        name="q"
+        defaultValue={query ?? ''}
+        placeholder="Buscar produtos"
         className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
       />
     </form>
