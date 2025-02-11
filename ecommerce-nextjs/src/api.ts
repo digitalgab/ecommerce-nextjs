b78475978ea5500e8ac3a47f7d0654c40204578c
Digital GaB <digitallgab@gmail.com>
2025-02-11 03:56:15 -0300

    feat: ajustes de variaveis de ambiente

--- ecommerce-nextjs/src/api.ts
+++ ecommerce-nextjs/src/api.ts
@@ -1,4 +1,4 @@
-import { env } from './utils/validate-env'
+import { env } from "./utils/validate-env"
 
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_URL
